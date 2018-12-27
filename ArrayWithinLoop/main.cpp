5c29f80834e32d0d3e472b8033ce55524738f809
Farhath Hussain <farhath.hussain17@bathspa.ac.uk>
2018-12-27 17:41:51 +0000

    Initial Commit

---
+++ ArrayWithinLoop/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ArrayWithinLoop
+//
+//  Created by Farhath Hussain on 27/12/2018.
+//  Copyright © 2018 Farhath Hussain. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
